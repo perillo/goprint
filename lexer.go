16283b712984fc921ab2e257bd79e372feb6bc0d
Manlio Perillo <manlio.perillo@gmail.com>
2016-01-14 12:56:06 +0100

    Update lexer to return more useful informations
    The Token type emitted by the lexer contained the chunk field, with source code
    around a Go token. However this data is not convenient to use.
    
    Update the Token type by replacing the chunk field with the code and whitespace
    fields.  The code contains well defined, consistent and documented data.
    
    Improve documentation.

--- lexer.go
+++ lexer.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 //
-// Support for scanning Go source files.
+// lexer.go source file is responsible for scanning Go source files.
 
 package main
 
@@ -14,9 +14,19 @@ import (
 type Token struct {
 	off  int
 	line int
-	// chunk is the source code chunk for the token, including whitespaces.
-	chunk string
-	value token.Token
+	// code is the token source code.
+	// For keywords, identifiers and basic type literals, it is the token
+	// literal.
+	// For the auto inserted SEMICOLON operator, it is "\n".
+	// For operators, it is the operator string representation.
+	// For raw strings literals and general comments, carriage return
+	// characters ('\r) are discarded.
+	code string
+	// whitespace is white space after the token.
+	// It contains only spaces (U+0020), horizontal tabs (U+0009), and
+	// newlines (U+000A).
+	whitespace string
+	value      token.Token
 }
 
 type lexer struct {
@@ -35,9 +45,18 @@ func (l *lexer) run() {
 			return
 		}
 		pos := l.file.Position(p)
+		if lit == "" {
+			// Operator token, excluding SEMICOLON.
+			lit = tok.String()
+		} else if lit == "\n" {
+			// The auto inserted SEMICOLON token.
+			// Add the newline to wsp, instead of code.
+			lit = ""
+		}
 		l.tokens <- &Token{
 			off:   pos.Offset,
 			line:  pos.Line,
+			code:  lit,
 			value: tok,
 		}
 	}
@@ -57,17 +76,17 @@ func scan(name string, input []byte) chan *Token {
 		tokens: in,
 	}
 
-	// In the first stage we collect tokens and their offset in the source
-	// code.
+	// In the first stage we collect tokens, their literal code and their
+	// offset in the source code.
 	go l.run()
 
-	// In the second stage we add source chunk for each token.
+	// In the second stage we add white space after each token.
 	out := make(chan *Token)
 
 	go func() {
 		prev := <-in
 		for cur := range in {
-			prev.chunk = l.input[prev.off:cur.off]
+			prev.whitespace = l.input[prev.off+len(prev.code) : cur.off]
 			out <- prev
 			prev = cur
 		}
