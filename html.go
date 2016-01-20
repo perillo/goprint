9081e53ba72ad045f83766b21fdcb5fdc870637c
Manlio Perillo <manlio.perillo@gmail.com>
2016-01-20 18:10:00 +0100

    Move getClass function to goefmt
    Despite the name, the getClass function is not limited to HTML.
    
    Move the getClass function to the goefmt package, and rename it to TokenClass.

--- html.go
+++ html.go
@@ -8,7 +8,6 @@ package main
 
 import (
 	"fmt"
-	"go/token"
 	"html"
 	"strings"
 
@@ -21,7 +20,7 @@ func spanToHTML(s *goefmt.Span) string {
 		// Only horizontal white space.
 		return s.Whitespace
 	}
-	class := strings.Join(getClass(s), " ")
+	class := strings.Join(goefmt.TokenClass(s), " ")
 	code := html.EscapeString(s.Code)
 
 	return fmt.Sprintf(
@@ -42,73 +41,4 @@ func lineToHTML(l goefmt.Line) string {
 	}
 
 	return strings.Join(spans, "")
-}
-
-// getClass returns the HTML class for the specified code span.
-func getClass(span *goefmt.Span) []string {
-	// Avoid extra allocation.
-	class := make([]string, 0, 2)
-
-	switch {
-	case span.Token.IsKeyword():
-		class = append(class, "keyword")
-	case span.Token.IsLiteral():
-		switch span.Token {
-		case token.IDENT:
-			class = append(class, "ident")
-			if isBuiltin(span.Code) {
-				// NOTE(mperillo): We ignore shadowing.
-				class = append(class, "builtin")
-			}
-		case token.INT:
-			class = append(class, "literal", "int")
-		case token.FLOAT:
-			class = append(class, "literal", "float")
-		case token.IMAG:
-			class = append(class, "literal", "imag")
-		case token.CHAR:
-			class = append(class, "literal", "char")
-		case token.STRING:
-			class = append(class, "literal", "string")
-		default:
-			panic("invalid literal token: " + span.Token.String())
-		}
-	case span.Token.IsOperator():
-		class = append(class, "operator")
-	case span.Token == token.COMMENT:
-		class = append(class, "comment")
-	case span.Token == token.ILLEGAL:
-		// TODO(mperillo): Handle it in the lexer?
-		class = append(class, "invalid")
-	default:
-		panic("unknown token type: " + span.Token.String())
-	}
-
-	return class
-}
-
-// Go predeclared identifiers.
-var builtins = []string{
-	// constants
-	"true", "false", "iota",
-	// variables
-	"nil",
-	// functions
-	"append", "cap", "close", "complex", "copy", "delete", "imag", "len",
-	"make", "new", "panic", "print", "println", "real", "recover",
-	// types
-	"bool", "byte", "complex128", "complex64", "error", "float32", "float64",
-	"int", "int16", "int32", "int64", "int8", "rune", "string", "uint",
-	"uint16", "uint32", "uint64", "uint8", "uintptr",
-}
-
-// isBuiltin returns true if the identifier is builtin.
-func isBuiltin(ident string) bool {
-	for _, ent := range builtins {
-		if ident == ent {
-			return true
-		}
-	}
-
-	return false
 }
