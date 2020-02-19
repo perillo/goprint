06665399a8c8101d4d29778e10447d0d199e444e
Manlio Perillo <manlio.perillo@gmail.com>
2020-02-19 12:14:02 +0100

    Move HTML formatting code to html.go
    Move the File type and the render, spanToHTML and lineToHTML functions
    to a new html.go source file.

---
+++ html.go
@@ -0,0 +1,69 @@
+// Copyright 2020 Manlio Perillo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html"
+	"html/template"
+	"strings"
+
+	"github.com/perillo/goprint/internal/goefmt"
+)
+
+// File represents an HTML formatted Go source file.
+type File struct {
+	Name string
+	Code template.HTML
+}
+
+// render returns an HTML fragment containing the formatted Go code for the
+// specified source file. A line number is printed at the begin of each line.
+func render(name string, input []byte) template.HTML {
+	buf := new(bytes.Buffer)
+
+	n := 1
+	for line := range goefmt.Format(goefmt.Scan(name, input)) {
+		if line == nil {
+			// Empty line
+			fmt.Fprintf(buf, "<span class=\"line empty\">%3d</span>\n", n)
+		} else {
+			fmt.Fprintf(buf, "<span class=\"line\">%3d</span> %s\n", n,
+				lineToHTML(line))
+		}
+		n++
+	}
+
+	return template.HTML(buf.String())
+}
+
+// spanToHTML returns an HTML representation for the code span.
+func spanToHTML(s *goefmt.Span) string {
+	if s.Code == "" {
+		// Only horizontal white space.
+		return s.Whitespace
+	}
+	class := strings.Join(goefmt.TokenClass(s), " ")
+	code := html.EscapeString(s.Code)
+
+	return fmt.Sprintf(`<span class="%s">%s</span>%s`, class, code, s.Whitespace)
+}
+
+// lineToHTML returns an HTML representation for the code line. The eol is not
+// included.
+func lineToHTML(l goefmt.Line) string {
+	if l == nil {
+		// Empty line.
+		return ""
+	}
+
+	spans := make([]string, len(l))
+	for i, span := range l {
+		spans[i] = spanToHTML(span)
+	}
+
+	return strings.Join(spans, "")
+}
--- main.go
+++ main.go
@@ -9,27 +9,17 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
-	"html"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/perillo/goprint/internal/css"
-	"github.com/perillo/goprint/internal/goefmt"
 	"github.com/perillo/goprint/internal/packages"
 )
-
-// File represents an HTML formatted Go source file.
-type File struct {
-	Name string
-	Code template.HTML
-}
 
 // Context is the context used by the HTML template.
 type Context struct {
@@ -145,52 +135,4 @@ func build(pkg *packages.Package, test bool) ([]File, error) {
 	}
 
 	return files, nil
-}
-
-// render returns an HTML fragment containing the formatted Go code for the
-// specified source file. A line number is printed at the begin of each line.
-func render(name string, input []byte) template.HTML {
-	buf := new(bytes.Buffer)
-
-	n := 1
-	for line := range goefmt.Format(goefmt.Scan(name, input)) {
-		if line == nil {
-			// Empty line
-			fmt.Fprintf(buf, "<span class=\"line empty\">%3d</span>\n", n)
-		} else {
-			fmt.Fprintf(buf, "<span class=\"line\">%3d</span> %s\n", n,
-				lineToHTML(line))
-		}
-		n++
-	}
-
-	return template.HTML(buf.String())
-}
-
-// spanToHTML returns an HTML representation for the code span.
-func spanToHTML(s *goefmt.Span) string {
-	if s.Code == "" {
-		// Only horizontal white space.
-		return s.Whitespace
-	}
-	class := strings.Join(goefmt.TokenClass(s), " ")
-	code := html.EscapeString(s.Code)
-
-	return fmt.Sprintf(`<span class="%s">%s</span>%s`, class, code, s.Whitespace)
-}
-
-// lineToHTML returns an HTML representation for the code line. The eol is not
-// included.
-func lineToHTML(l goefmt.Line) string {
-	if l == nil {
-		// Empty line.
-		return ""
-	}
-
-	spans := make([]string, len(l))
-	for i, span := range l {
-		spans[i] = spanToHTML(span)
-	}
-
-	return strings.Join(spans, "")
 }
